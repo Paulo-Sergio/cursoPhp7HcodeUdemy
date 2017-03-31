c145390d0dd1ffa9e9458c0349babaffa5d8c445
Paulo-Sergio <paulofranca.contato@gmail.com>
2017-03-31 10:23:04 -0300

    Criando classe de modelo Usuario.php

--- PDO-DAO/index.php
+++ PDO-DAO/index.php
@@ -2,7 +2,6 @@
 
 require_once './config.php';
 
-$sql = new Sql();
-$usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
-echo json_encode($usuarios);
+$usuario = new Usuario();
+$usuario->loadById(5);
+echo $usuario;
